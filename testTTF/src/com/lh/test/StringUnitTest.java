0a05ff2bce652dab80e288072b9fd3719da68eb0
lvhui1973 <13801220173@139.com>
2015-08-11 07:12:53 +0800

    test version of testCase

---
+++ testTTF/src/com/lh/test/StringUnitTest.java
@@ -0,0 +1,31 @@
+/**
+ * StringUnitTest.java,created on 2015Äê8ÔÂ8ÈÕ
+ * Copyright lh,All right reserved.
+ * @author by lh
+ *
+ */
+package com.lh.test;
+
+import junit.framework.TestCase;
+
+public class StringUnitTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	public void testSplit()
+	{
+		System.out.println("In Testsplit");
+		
+		
+	}
+	public void testSplitException()
+	{
+		
+	}
+
+}
